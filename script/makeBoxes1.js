26a7a250def1641c30ae4d782a7fd558140421f8
hexa-coding <87714098+hexa-coding@users.noreply.github.com>
2022-08-29 13:38:42 +0200

    set-up jquery

---
+++ script/makeBoxes1.js
@@ -0,0 +1,13 @@
+function makeBoxes1() {
+  //Create a 8x3 grid of html elements with jQuery
+
+  $("#live-print-area").hide();
+
+  // $(document).ready(function ()
+  // {
+  //   $("button").click(function ()
+  //   {
+  //     $("#live-print-area").hide();
+  //   });
+  // });
+}
